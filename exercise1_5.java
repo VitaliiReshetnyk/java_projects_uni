db6aaae9e72939f95f486a0820961b6e49f5bbac
VitaliiReshetnyk <113349766+VitaliiReshetnyk@users.noreply.github.com>
2023-09-10 18:15:25 +0300

    Vitalii Reshetnyk compmath1 3year, ex 1.1-1.9

---
+++ exercise1_5.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/**
+ * exercise1_5
+ * @author Vitalii Reshetnyk
+ */public class exercise1_5
+{
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
+        
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+        int z = scanner.nextInt();
+
+        System.out.println("x=" + x + ", y=" + y +", z=" + z + "\n");
+        scanner.close();
+    }
+}
